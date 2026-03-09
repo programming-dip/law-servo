ed9e3af3d31a62cd51d6482f620306e25d134746
programming-dip <anudipbhoumik@gmail.com>
2026-03-10 01:47:50 +1100

    loading home

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Banner from '../../Banner/Banner';
 import Lawyers from '../../Lawyers/Lawyers';
 import Success from '../../Success/Success';
 import LawyerDetails from '../LawyerDetails/LawyerDetails';
+import Loading from '../../Loading/Loading';
 
 export const dataContext = createContext('');
 
@@ -11,19 +12,28 @@ export const dataContext = createContext('');
 
 const Home = () => {
     const [lawyersData, setLawyersData] = useState([]);
+    const [isDataLoading, setIsDataLoading] = useState(true);
 
     
     useEffect(() => {
         fetch('/lawyerData.json')
             .then(res => res.json())
-            .then(data => setLawyersData(data));
+            .then(data => {
+                setLawyersData(data);
+                setIsDataLoading(false);
+            });
+
     }, [])
 
     return (
+
         <div className='px-5 md:px-12 max-w-[90vw] mx-auto'>
             <Banner></Banner>
             <dataContext.Provider value={lawyersData}>
-                <Lawyers></Lawyers>
+                {
+                    isDataLoading? <Loading></Loading> :
+                    <Lawyers></Lawyers>
+                }
             </dataContext.Provider>
             <Success></Success>
         </div>
